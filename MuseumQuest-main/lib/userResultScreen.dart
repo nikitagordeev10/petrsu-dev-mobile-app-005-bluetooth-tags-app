564f98f5e4e793234d2728f2a6686f5afbd69b2e
Екатерина <sokolova0ekaterina@gmail.com>
2024-04-20 08:45:15 +0300

    Commit changes before pull

--- MuseumQuest-main/lib/userResultScreen.dart
+++ MuseumQuest-main/lib/userResultScreen.dart
@@ -1,107 +1,66 @@
-// import 'package:flutter/material.dart';
-// import 'userMainScreen.dart';
-// class userResultScreen extends StatelessWidget {
-//   const userResultScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const Text('Результаты'),
-//             TextButton.icon(
-//               style: ButtonStyle(
-//                 backgroundColor: MaterialStateProperty.all(Colors.black),
-//                 foregroundColor: MaterialStateProperty.all(Colors.white),
-//               ),
-//               onPressed: () {
-//                 //Navigator.pop(context);
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context)
-//                     {
-//                       return const userMainScreen();
-//                     },
-//                   ),
-//                 );
-//               },
-//               icon: const Icon(Icons.arrow_circle_right_sharp),
-//               label: const Text('На главную'),
-//             ),
-//           ],
-//         ),
-//       ),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'userHomeScreen.dart';
-
-// import 'main.dart';
 
 class userResultScreen extends StatelessWidget {
   const userResultScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Результаты', style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-      ),
-      backgroundColor: const Color(0xFF1F1D2B), // Фоновый цвет
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0), // добавляем отступы
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'lib/img/results_image.png',
-                width: 200,
-                height: 200,
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Поздравляем! \nНа прохождение квеста\nвам потребовалось ≈ 15 минут',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox( // чтобы кнопка была на всю ширину
-                width: double.infinity,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const userHomeScreen()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF1AACBC), // цвет кнопки
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4), // закругление углов
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Результаты', style: TextStyle(color: Colors.white)),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+          ),
+          backgroundColor: const Color(0xFF1F1D2B), // Фоновый цвет
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0), // добавляем отступы
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'lib/img/results_image.png',
+                    width: 200,
+                    height: 200,
+                  ),
+                  const SizedBox(height: 20),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Поздравляем! \nНа прохождение квеста\nвам потребовалось ≈ 15 минут',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  child: const Text('Вернуться на главную', style: TextStyle(color: Colors.white)),
-                ),
-              ),
+                  const SizedBox(height: 20),
+                  SizedBox( // чтобы кнопка была на всю ширину
+                    width: double.infinity,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const userHomeScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF1AACBC), // цвет кнопки
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4), // закругление углов
+                        ),
+                      ),
+                      child: const Text('Вернуться на главную', style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
 
-            ],
+                ],
+              ),
+            ),
           ),
         ),
-      ),
     );
   }
 }
